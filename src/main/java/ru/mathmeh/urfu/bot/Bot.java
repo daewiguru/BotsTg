c1948a1548b03dac43d2dc6e17fc90d2c65ab8e0
lendys <aroslavrpisepnyj@gmail.com>
2023-10-14 18:36:51 +0500

    add logic and logic test refactor tgbot add bot.java

---
+++ src/main/java/ru/mathmeh/urfu/bot/Bot.java
@@ -0,0 +1,6 @@
+package ru.mathmeh.urfu.bot;
+
+public interface Bot {
+    void sendMessage(Long id, String message);
+}
+
---
+++ src/test/java/ru/mathmeh/urfu/bot/LogicTest.java
@@ -0,0 +1,44 @@
+package ru.mathmeh.urfu.bot;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ *class LogicTest
+ * @author lendys(Yaroslav Prisepnyj)
+ * testing commands \start and \help
+ */
+
+
+public class LogicTest {
+    /**
+     * method testStartCommand
+     * testing commands \start
+     */
+    @Test
+    public void testStartCommand() {
+        Logic logic = new Logic();
+        String result = logic.messageHandler("/start");
+        Assertions.assertEquals("Hello, im new bots", result);
+    }
+    /**
+     * method testHelpCommand
+     * testing commands \help
+     */
+    @Test
+    public void testHelpCommand() {
+        Logic logic = new Logic();
+        String result = logic.messageHandler("/help");
+        Assertions.assertEquals("I'm a bot that mirrors a message.", result);
+    }
+
+    /**
+     * method testDefaultCase
+     * testing commands default
+     */
+    @Test
+    public void testDefaultCase() {
+        Logic logic = new Logic();
+        String result = logic.messageHandler("Some other message");
+        Assertions.assertEquals("Some other message", result);
+    }
+}
