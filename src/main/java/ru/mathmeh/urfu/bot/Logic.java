d637f0735909e41a33bca288c4a39f98fdd69c60
Ярослав <110774623+Lendysgo@users.noreply.github.com>
2023-10-25 00:00:25 +0500

    Revert "Task2 yaroslav"

--- src/main/java/ru/mathmeh/urfu/bot/Logic.java
+++ src/main/java/ru/mathmeh/urfu/bot/Logic.java
@@ -1,84 +1,32 @@
 package ru.mathmeh.urfu.bot;
 
-import java.util.List;
-
 /**
- *  * The Logic class handles user commands and manages notes.
- *  * This class is essentially the controller of the bot, as it receives
- *  * user input, processes it, interacts with the NoteManager to manipulate
- *  * data, and then forms an appropriate response. The user commands it can handle
- *  * include: /start, /help, /table, /add, /edit, /del and other unsupported cases.
+ * This class implements the bot logic
+ * @author lendys(Yaroslav Prisepnyj)
+ * @version 1.0
  */
 public class Logic {
-    private NoteManager noteManager;
 
-    /**
-     * /**
-     *      * Constructor for the Logic class.
-     *      * Initializes a new NoteManager object that'll be used for managing the notes.
-     *      */
+    public Logic(){
 
-    public Logic() {
-        noteManager = new NoteManager();
     }
 
     /**
-     * Handles user messages and performs actions based on the command.
-     *
-     * @param message The user's message.
-     * @return The bot's response.
+     * This method realize cross-platform logic of bots
+     * @param message text of user`s message
+     * @return tet of bot message
      */
-    public String handleMessage(String message) {
-        String[] parts = message.split(" ");
-        String command = parts[0];
-
-        switch (command) {
-            case "/start":
-                return "Hello, I'm a simple note bot.";
-            case "/help":
-                return "I'm a bot for managing notes. You can add, edit, delete, and view your notes.";
-            case "/table":
-                List<Note> notes = noteManager.getNotes();
-                StringBuilder response = new StringBuilder("Your notes:\n");
-                for (Note note : notes) {
-                    response.append(note.getId()).append(". ").append(note.getText()).append("\n");
-                }
-                return response.toString();
-            case "/add":
-                if (parts.length >= 2) {
-                    String text = message.substring(command.length() + 1);
-                    noteManager.addNote(text);
-                    return "Note added!";
-                } else {
-                    return "Please provide a text for the note.";
-                }
-            case "/edit":
-                if (parts.length >= 2) {
-                    try {
-                        int id = Integer.parseInt(parts[1]);
-                        String text = message.substring(command.length() + 2 + parts[1].length());
-                        noteManager.editNote(id, text);
-                        return "Note edited!";
-                    } catch (NumberFormatException e) {
-                        return "Invalid note ID.";
-                    }
-                } else {
-                    return "Please provide a note ID and text for editing.";
-                }
-            case "/del":
-                if (parts.length >= 2) {
-                    try {
-                        int id = Integer.parseInt(parts[1]);
-                        noteManager.deleteNote(id);
-                        return "Note deleted!";
-                    } catch (NumberFormatException e) {
-                        return "Invalid note ID.";
-                    }
-                } else {
-                    return "Please provide a note ID for deletion.";
-                }
-            default:
-                return "I don't understand the command.";
+    public String handleMessage(String message){
+        switch (message){
+            case "/start" -> {
+                return "Hello, im new bots";
+            }
+            case "/help" -> {
+                return "I'm a bot that mirrors a message.";
+            }
+            default -> {
+                return message;
+            }
         }
     }
 }
